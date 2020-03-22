ec77c53b26a5ce105a6ea40e7e5bcdf2443be3ef
Donny Wu <enderpicture@gmail.com>
2020-03-21 20:06:17 -0700

    remove non essentials

--- video/src/video/Video.java
+++ video/src/video/Video.java
@@ -23,29 +23,24 @@ import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
 
 public class Video {
-	
-	
-	
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         String basePath = new File("").getAbsolutePath();
         System.out.println("basePath " + basePath);
 
         String filePath = "\\1.avi";
-        String newPath=(basePath + filePath);
+        String newPath = (basePath + filePath);
         String output = (basePath + "\\output\\output.mp4");
         String finalOutput = (basePath + "\\output");
         double FRAME_RATE = 50;
         int SECONDS_TO_RUN_FOR = 20;
-        
-        System.out.println("newPath " + newPath);
-        
-       
 
-        if (!Paths.get(newPath).toFile().exists()){
-             System.out.println("File " + filePath + " does not exist!");
-             return;
+        System.out.println("newPath " + newPath);
+
+        if (!Paths.get(newPath).toFile().exists()) {
+            System.out.println("File " + filePath + " does not exist!");
+            return;
         }
 
         VideoCapture camera = new VideoCapture(newPath);
@@ -55,113 +50,106 @@ public class Video {
             return;
         }
         Mat frame = new Mat();
-        
+
         int video_length = (int) camera.get(Videoio.CAP_PROP_FRAME_COUNT);
         int frame_number = (int) camera.get(Videoio.CAP_PROP_POS_FRAMES);
 
-       //save frame as jpg
-        while(camera.read(frame)) {
-            Imgcodecs.imwrite(basePath +"\\output\\" + frame_number +".jpg", frame);
+        // save frame as jpg
+        while (camera.read(frame)) {
+            Imgcodecs.imwrite(basePath + "\\output\\" + frame_number + ".jpg", frame);
             frame_number++;
-            if (frame_number == video_length-1) {
-            	break;
+            if (frame_number == video_length - 1) {
+                break;
             }
         }
-        
-        
-        //reading the frames
+
+        // reading the frames
         System.out.println(video_length + " Frames extracted");
 
         BufferedImage bufferedImage = matToBufferedImage(frame);
         showWindow(bufferedImage);
         camera.release();
-        
-        
-        //read the previously extracted frames that were saved and load it in the array
-        BufferedImage[] bImg = new BufferedImage[video_length]; 
-    	for (int i=0; i<video_length-1;i++) {
-    		try {
-    			bImg[i] = ImageIO.read(new File(basePath +"\\output\\"+i+".jpg"));
 
-    		} catch (Exception e) {
-    			System.out.println("Cannot load the provided image");
-    		}
-    	}
-    	
-    	
-    	//save file as image
-//    	File outputfile;
-//    	for (int x = 0; x<bImg.length-1;x++) {
-//    		outputfile = new File(basePath +"\\output\\"+x+"s.png");
-//    		//use subtract method for images
-//        	ImageIO.write(subtract(bImg[1].getHeight(), bImg[1].getWidth(), bImg[1].getType(), bImg.length, bImg, x)
-//        			, "png", outputfile);
-//
-//    	}
-//    	
-//    	
-//    	//method to save images as movie
-//    	convertJPGtoMovie(bImg, basePath +"\\output\\");
-//    	
-    	
-    	
+        // read the previously extracted frames that were saved and load it in the array
+        BufferedImage[] bImg = new BufferedImage[video_length];
+        for (int i = 0; i < video_length - 1; i++) {
+            try {
+                bImg[i] = ImageIO.read(new File(basePath + "\\output\\" + i + ".jpg"));
+
+            } catch (Exception e) {
+                System.out.println("Cannot load the provided image");
+            }
+        }
+
+        // save file as image
+        // File outputfile;
+        // for (int x = 0; x<bImg.length-1;x++) {
+        // outputfile = new File(basePath +"\\output\\"+x+"s.png");
+        // //use subtract method for images
+        // ImageIO.write(subtract(bImg[1].getHeight(), bImg[1].getWidth(),
+        // bImg[1].getType(), bImg.length, bImg, x)
+        // , "png", outputfile);
+        //
+        // }
+        //
+        //
+        // //method to save images as movie
+        // convertJPGtoMovie(bImg, basePath +"\\output\\");
+        //
+
     }
-	
-	
-	public BufferedImage subtract(int height, int width, int imageType, int length, BufferedImage[] src, int x) {
-		BufferedImage result = new BufferedImage(width, height, imageType);
-		
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height ; j++) {
-				
-				int red = getRed(src[x].getRGB(i, j))-getRed(src[x].getRGB(i, j));
-				int green = getGreen(src[x].getRGB(i, j))-getGreen(src[x].getRGB(i, j));
-				int blue = getBlue(src[x].getRGB(i, j))-getBlue(src[x].getRGB(i, j));
-		result.setRGB(i, j, new Color(red, green, blue).getRGB());
 
-			}}
-		
-		return result;
-	}
-	
+    public BufferedImage subtract(int height, int width, int imageType, int length, BufferedImage[] src, int x) {
+        BufferedImage result = new BufferedImage(width, height, imageType);
 
-//	
-	protected int getRed(int pixel) {
-		return (pixel >>> 16) & 0xFF;
-	}
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
 
-	protected int getGreen(int pixel) {
-		return (pixel >>> 8) & 0xFF;
-	}
+                int red = getRed(src[x].getRGB(i, j)) - getRed(src[x].getRGB(i, j));
+                int green = getGreen(src[x].getRGB(i, j)) - getGreen(src[x].getRGB(i, j));
+                int blue = getBlue(src[x].getRGB(i, j)) - getBlue(src[x].getRGB(i, j));
+                result.setRGB(i, j, new Color(red, green, blue).getRGB());
 
-	protected int getBlue(int pixel) {
-		return pixel & 0xFF;
-	}
-    
-    public static void convertJPGtoMovie(BufferedImage[] links, String vidPath)  
-    {  
-    	
-    	FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(vidPath+ "video" +".mpeg",640,720);  
-        try {  
-             recorder.setFrameRate(1);  
-    recorder.setVideoCodec(avcodec.AV_CODEC_ID_MPEG4);  
-    recorder.setVideoBitrate(9000);  
-    recorder.setFormat("mpeg");  
-    recorder.setVideoQuality(0); // maximum quality  
-    recorder.start();  
-          for (int i=0;i<links.length;i++)  
-          {  
-        	Java2DFrameConverter converter1 = new Java2DFrameConverter();
+            }
+        }
 
-        	  recorder.record(converter1.convert(links[i]));
-          }  
-          recorder.stop();  
-         }  
-         catch (org.bytedeco.javacv.FrameRecorder.Exception e){  
-           e.printStackTrace();  
-         }  
+        return result;
+    }
 
-    }  
+    //
+    protected int getRed(int pixel) {
+        return (pixel >>> 16) & 0xFF;
+    }
+
+    protected int getGreen(int pixel) {
+        return (pixel >>> 8) & 0xFF;
+    }
+
+    protected int getBlue(int pixel) {
+        return pixel & 0xFF;
+    }
+
+    public static void convertJPGtoMovie(BufferedImage[] links, String vidPath) {
+
+        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(vidPath + "video" + ".mpeg", 640, 720);
+        try {
+            recorder.setFrameRate(1);
+            recorder.setVideoCodec(avcodec.AV_CODEC_ID_MPEG4);
+            recorder.setVideoBitrate(9000);
+            recorder.setFormat("mpeg");
+            recorder.setVideoQuality(0); // maximum quality
+            recorder.start();
+            for (int i = 0; i < links.length; i++) {
+                Java2DFrameConverter converter1 = new Java2DFrameConverter();
+
+                recorder.record(converter1.convert(links[i]));
+            }
+            recorder.stop();
+        } catch (org.bytedeco.javacv.FrameRecorder.Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     private static BufferedImage matToBufferedImage(Mat frame) {
         int type = 0;
@@ -183,10 +171,9 @@ public class Video {
         JFrame frame = new JFrame();
         frame.getContentPane().add(new JLabel(new ImageIcon(img)));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(img.getWidth()/2, img.getHeight()/2 + 30);
+        frame.setSize(img.getWidth() / 2, img.getHeight() / 2 + 30);
         frame.setTitle("Image captured");
         frame.setVisible(true);
     }
-    
- 
+
 }
