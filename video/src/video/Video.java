485be6f64f08e8bc36939cfc62c212b2d73c0080
Donny Wu <enderpicture@gmail.com>
2020-03-22 16:06:05 -0700

    working subtract func

--- video/src/video/Video.java
+++ video/src/video/Video.java
@@ -53,7 +53,7 @@ public class Video {
             return;
         }
 
-        ArrayList<BufferedImage> frames = new ArrayList<>();
+        ArrayList<BufferedImage> sourceFrames = new ArrayList<>();
         int frameCount = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_COUNT);
         double fps = videoCapture.get(Videoio.CAP_PROP_FPS);
         int width = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH);
@@ -66,44 +66,58 @@ public class Video {
         // read in all frames
         Mat frame = new Mat();
         while (videoCapture.read(frame)) {
-            frames.add(matToBuf(frame));
+            sourceFrames.add(matToBuf(frame));
         }
         videoCapture.release();
 
         // process frames
+        ArrayList<BufferedImage> outputFrames = new ArrayList<>();
+        for (int i = 0; i < sourceFrames.size() - 1; i++) {
+            BufferedImage frame1 = sourceFrames.get(i);
+            BufferedImage frame2 = sourceFrames.get(i + 1);
+
+            outputFrames.add(subtract(frame1, frame2));
+        }
 
         // save video
-        // FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(basePath + "\\video" +
-        // ".mpeg", width, height);
-        // recorder.setFrameRate(fps);
-        // recorder.setVideoCodec(avcodec.AV_CODEC_ID_MPEG4);
-        // recorder.setVideoBitrate(9000);
-        // recorder.setFormat("mpeg");
-        // recorder.setVideoQuality(0); // maximum quality
-        // try {
-        // recorder.start();
-        // Java2DFrameConverter converter1 = new Java2DFrameConverter();
-        // Iterator<BufferedImage> iterator = frames.iterator();
-        // while (iterator.hasNext()) {
-        // recorder.record(converter1.convert(iterator.next()));
-        // }
-        // recorder.stop();
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // }
-
-        System.out.println(frames.get(0).getWidth());
-        System.out.println(frames.get(0).getHeight());
-
         VideoWriter videoWriter = new VideoWriter("1output.avi", VideoWriter.fourcc('x', '2', '6', '4'), fps,
                 new Size(width, height));
 
-        Iterator<BufferedImage> iterator = frames.iterator();
+        Iterator<BufferedImage> iterator = outputFrames.iterator();
         while (iterator.hasNext()) {
             Mat mat = bufToMat(iterator.next());
             videoWriter.write(mat);
         }
         videoWriter.release();
+    }
+
+    public BufferedImage subtract(BufferedImage src1, BufferedImage src2) {
+        int width = src1.getWidth();
+        int height = src1.getHeight();
+
+        BufferedImage out = new BufferedImage(width, height, src1.getType());
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color pixel1 = new Color(src1.getRGB(x, y));
+                Color pixel2 = new Color(src2.getRGB(x, y));
+
+                int r = pixel1.getRed() - pixel2.getRed();
+                int g = pixel1.getGreen() - pixel2.getGreen();
+                int b = pixel1.getBlue() - pixel2.getBlue();
+
+                Color pixelOut = new Color(clamp(r), clamp(g), clamp(b));
+
+                out.setRGB(x, y, pixelOut.getRGB());
+            }
+        }
+        return out;
+    }
+
+    public int clamp(int in) {
+        in = in > 255 ? 255 : in;
+        in = in < 0 ? 0 : in;
+        return in;
     }
 
     /**
@@ -143,13 +157,13 @@ public class Video {
         return out;
     }
 
-    // private static void showWindow(BufferedImage img) {
-    // JFrame frame = new JFrame();
-    // frame.getContentPane().add(new JLabel(new ImageIcon(img)));
-    // frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    // frame.setSize(img.getWidth() / 2, img.getHeight() / 2 + 30);
-    // frame.setTitle("Image captured");
-    // frame.setVisible(true);
-    // }
+    private static void showWindow(BufferedImage img) {
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(img.getWidth() / 2, img.getHeight() / 2 + 30);
+        frame.setTitle("Image captured");
+        frame.setVisible(true);
+    }
 
 }
