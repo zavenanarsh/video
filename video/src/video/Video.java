1b812f97745f05e9e4f9ca47260478b5cfb9292f
Donny Wu <enderpicture@gmail.com>
2020-04-02 22:52:26 -0700

    working on it

--- video/src/video/Video.java
+++ video/src/video/Video.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -43,195 +44,202 @@ import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
 
 public class Video {
-	
-boolean removeRed, removeGreen, removeBlue;
 
+	boolean removeRed, removeGreen, removeBlue;
+	int width, height, frameCount, currentFrame;
+	double fps;
 
-    public static void main(String[] args) {
-        new Video();
-    }
+	ArrayList<BufferedImage> outputFrames;
+	ArrayList<BufferedImage> sourceFrames;
 
-    Video() {
-    	
-    	UI();
-    	
-    	    			
-//        System.out.println("Starting program --------------------------");
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//        String basePath = new File("").getAbsolutePath();
-//
-//        String fileName = "2.avi";
-//        String filePath = (basePath + "\\" + fileName);
-//        System.out.println("Attempting to load: " + fileName);
-//        System.out.println("At file path: " + filePath);
-//
-//        // check if file exists
-//        if (!Paths.get(filePath).toFile().exists()) {
-//            System.out.println(fileName + " does not exit!");
-//            return;
-//        }
-//
-//        VideoCapture videoCapture = new VideoCapture(filePath);
-//        if (!videoCapture.isOpened()) {
-//            System.out.println("Error! file can't be opened!");
-//            return;
-//        }
-//        System.out.println("File read successful --------------------------");
-//
-//        ArrayList<BufferedImage> sourceFrames = new ArrayList<>();
-//        int frameCount = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_COUNT);
-//        double fps = videoCapture.get(Videoio.CAP_PROP_FPS);
-//        int width = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH);
-//        int height = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT);
-//        int currentFrame = 0;
-//        System.out.println("File info:");
-//        System.out.println("\twidth: " + width);
-//        System.out.println("\theight: " + height);
-//        System.out.println("\tfps: " + fps);
-//        System.out.println("\tnumber of frames: " + frameCount);
-//
-//        // read in all frames
-//        System.out.println("Starting to read individual frames --------------------------");
-//        Mat frame = new Mat();
-//        while (videoCapture.read(frame)) {
-//            sourceFrames.add(matToBuf(frame));
-//            currentFrame++;
-//            System.out.println("Reading frame " + currentFrame + " of " + frameCount);
-//        }
-//        videoCapture.release();
-//        System.out.println("Done!");
-//
-//        // process frames
-//        System.out.println("Starting to process frames --------------------------");
-//        ArrayList<BufferedImage> outputFrames = new ArrayList<>();
-//        for (int i = 0; i < sourceFrames.size() - 1; i++) {
-//            BufferedImage frame1 = sourceFrames.get(i);
-//            BufferedImage frame2 = sourceFrames.get(i + 1);
-//
-//            outputFrames.add(subtract(frame1, frame2));
-//
-//            System.out.println("Processing frame " + (i+1) + " of " + (sourceFrames.size() - 1));
-//        }
-//        System.out.println("Done!");
-//
-//        // save video
-//        System.out.println("Saving video --------------------------");
-//        VideoWriter videoWriter = new VideoWriter("1output.avi", VideoWriter.fourcc('x', '2', '6', '4'), fps,
-//                new Size(width, height));
-//
-//        Iterator<BufferedImage> iterator = outputFrames.iterator();
-//        while (iterator.hasNext()) {
-//            Mat mat = bufToMat(iterator.next());
-//            videoWriter.write(mat);
-//        }
-//        videoWriter.release();
-//        System.out.println("Done!");
-    }
+	public static void main(String[] args) {
+		new Video();
+	}
 
-    public BufferedImage subtract(BufferedImage src1, BufferedImage src2) {
-        int width = src1.getWidth();
-        int height = src1.getHeight();
+	Video() {
 
-        BufferedImage out = new BufferedImage(width, height, src1.getType());
+		UI();
+	}
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                Color pixel1 = new Color(src1.getRGB(x, y));
-                Color pixel2 = new Color(src2.getRGB(x, y));
+	public void loadVideo() {
 
-                int r = pixel1.getRed() - pixel2.getRed();
-                int g = pixel1.getGreen() - pixel2.getGreen();
-                int b = pixel1.getBlue() - pixel2.getBlue();
+		System.out.println("Starting program --------------------------");
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		String basePath = new File("").getAbsolutePath();
 
-                Color pixelOut = new Color(clamp(r), clamp(g), clamp(b));
+		String fileName = "2.avi";
+		String filePath = (basePath + "\\" + fileName);
+		System.out.println("Attempting to load: " + fileName);
+		System.out.println("At file path: " + filePath);
 
-                out.setRGB(x, y, pixelOut.getRGB());
-            }
-        }
-        return out;
-    }
-    
-   
-    public int clamp(int in) {
-        in = in > 255 ? 255 : in;
-        in = in < 0 ? 0 : in;
-        return in;
-    }
+		// check if file exists
+		if (!Paths.get(filePath).toFile().exists()) {
+			System.out.println(fileName + " does not exit!");
+			return;
+		}
 
-    /**
-     * https://riptutorial.com/opencv/example/21963/converting-an-mat-object-to-an-bufferedimage-object
-     * 
-     * @param in
-     * @return
-     */
-    private BufferedImage matToBuf(Mat in) {
-        if (!in.empty()) {
-            int type = BufferedImage.TYPE_BYTE_GRAY;
-            if (in.channels() > 1) {
-                type = BufferedImage.TYPE_3BYTE_BGR;
-            }
-            int bufferSize = in.channels() * in.cols() * in.rows();
-            byte[] b = new byte[bufferSize];
-            in.get(0, 0, b); // get all the pixels
-            BufferedImage out = new BufferedImage(in.cols(), in.rows(), type);
-            final byte[] targetPixels = ((DataBufferByte) out.getRaster().getDataBuffer()).getData();
-            System.arraycopy(b, 0, targetPixels, 0, b.length);
-            return out;
-        }
+		VideoCapture videoCapture = new VideoCapture(filePath);
+		if (!videoCapture.isOpened()) {
+			System.out.println("Error! file can't be opened!");
+			return;
+		}
+		System.out.println("File read successful --------------------------");
 
-        return null;
-    }
+		sourceFrames = new ArrayList<>();
+		frameCount = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_COUNT);
+		fps = videoCapture.get(Videoio.CAP_PROP_FPS);
+		width = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH);
+		height = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT);
+		currentFrame = 0;
+		System.out.println("File info:");
+		System.out.println("\twidth: " + width);
+		System.out.println("\theight: " + height);
+		System.out.println("\tfps: " + fps);
+		System.out.println("\tnumber of frames: " + frameCount);
 
-    /**
-     * https://stackoverflow.com/questions/14958643/converting-bufferedimage-to-mat-in-opencv
-     * 
-     * @param in
-     * @return
-     */
-    public static Mat bufToMat(BufferedImage in) {
-        Mat out = new Mat(in.getHeight(), in.getWidth(), CvType.CV_8UC3);
-        byte[] data = ((DataBufferByte) in.getRaster().getDataBuffer()).getData();
-        out.put(0, 0, data);
-        return out;
-    }
+		// read in all frames
+		System.out.println("Starting to read individual frames --------------------------");
+		Mat frame = new Mat();
+		while (videoCapture.read(frame)) {
+			sourceFrames.add(matToBuf(frame));
+			currentFrame++;
+			System.out.println("Reading frame " + currentFrame + " of " + frameCount);
+		}
+		videoCapture.release();
+		System.out.println("Done!");
+	}
 
-    public void difference(int r, int g, int b) {
-    	if (removeRed == true) {
-    		if (r > g) {
-    			r = g;
-    		}
-    	}
-    	
-    	if (removeGreen == true) {
-    		if (g > r) {
-    			g = r;
-    		}
-    	}
-    	
-    	if (removeBlue == true) {
-    		if (b > g) {
-    			b = g;
-    		}
-    	}
-    }
-    
-    public void UI() {
-    	
-    	JFrame frame = new JFrame();
-    	frame.setSize(1000, 500);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setTitle("Image captured");
-        
-        
-		
+	public void processVideo() {
+		// process frames
+		System.out.println("Starting to process frames --------------------------");
+		outputFrames = new ArrayList<>();
+		for (int i = 0; i < sourceFrames.size() - 1; i++) {
+			BufferedImage frame1 = sourceFrames.get(i);
+			BufferedImage frame2 = sourceFrames.get(i + 1);
+
+			outputFrames.add(subtract(frame1, frame2));
+
+			System.out.println("Processing frame " + (i + 1) + " of " + (sourceFrames.size() - 1));
+		}
+		System.out.println("Done!");
+	}
+
+	public void saveVideo() {
+		// save video
+		System.out.println("Saving video --------------------------");
+		VideoWriter videoWriter = new VideoWriter("1output.avi", VideoWriter.fourcc('x', '2', '6', '4'), fps,
+				new Size(width, height));
+
+		Iterator<BufferedImage> iterator = outputFrames.iterator();
+		while (iterator.hasNext()) {
+			Mat mat = bufToMat(iterator.next());
+			videoWriter.write(mat);
+		}
+		videoWriter.release();
+		System.out.println("Done!");
+	}
+
+	public BufferedImage subtract(BufferedImage src1, BufferedImage src2) {
+		int width = src1.getWidth();
+		int height = src1.getHeight();
+
+		BufferedImage out = new BufferedImage(width, height, src1.getType());
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				Color pixel1 = new Color(src1.getRGB(x, y));
+				Color pixel2 = new Color(src2.getRGB(x, y));
+
+				int r = pixel1.getRed() - pixel2.getRed();
+				int g = pixel1.getGreen() - pixel2.getGreen();
+				int b = pixel1.getBlue() - pixel2.getBlue();
+
+				Color pixelOut = new Color(clamp(r), clamp(g), clamp(b));
+
+				out.setRGB(x, y, pixelOut.getRGB());
+			}
+		}
+		return out;
+	}
+
+	public int clamp(int in) {
+		in = in > 255 ? 255 : in;
+		in = in < 0 ? 0 : in;
+		return in;
+	}
+
+	/**
+	 * https://riptutorial.com/opencv/example/21963/converting-an-mat-object-to-an-bufferedimage-object
+	 * 
+	 * @param in
+	 * @return
+	 */
+	private BufferedImage matToBuf(Mat in) {
+		if (!in.empty()) {
+			int type = BufferedImage.TYPE_BYTE_GRAY;
+			if (in.channels() > 1) {
+				type = BufferedImage.TYPE_3BYTE_BGR;
+			}
+			int bufferSize = in.channels() * in.cols() * in.rows();
+			byte[] b = new byte[bufferSize];
+			in.get(0, 0, b); // get all the pixels
+			BufferedImage out = new BufferedImage(in.cols(), in.rows(), type);
+			final byte[] targetPixels = ((DataBufferByte) out.getRaster().getDataBuffer()).getData();
+			System.arraycopy(b, 0, targetPixels, 0, b.length);
+			return out;
+		}
+
+		return null;
+	}
+
+	/**
+	 * https://stackoverflow.com/questions/14958643/converting-bufferedimage-to-mat-in-opencv
+	 * 
+	 * @param in
+	 * @return
+	 */
+	public static Mat bufToMat(BufferedImage in) {
+		Mat out = new Mat(in.getHeight(), in.getWidth(), CvType.CV_8UC3);
+		byte[] data = ((DataBufferByte) in.getRaster().getDataBuffer()).getData();
+		out.put(0, 0, data);
+		return out;
+	}
+
+	public void difference(int r, int g, int b) {
+		if (removeRed == true) {
+			if (r > g) {
+				r = g;
+			}
+		}
+
+		if (removeGreen == true) {
+			if (g > r) {
+				g = r;
+			}
+		}
+
+		if (removeBlue == true) {
+			if (b > g) {
+				b = g;
+			}
+		}
+	}
+
+	public void UI() {
+
+		JFrame frame = new JFrame();
+		frame.setSize(1000, 500);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setTitle("Image captured");
+		frame.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
+
 		JButton choose = new JButton("Choose Video");
 		JButton button = new JButton("Motion Detect!");
-		JCheckBox red = new JCheckBox("Red"); 
-		JCheckBox green = new JCheckBox("Green");  
-		JCheckBox blue = new JCheckBox("Blue");  
-		JLabel difference = new JLabel("Select a colour to use the difference colour method.");  
-
+		JCheckBox red = new JCheckBox("Red");
+		JCheckBox green = new JCheckBox("Green");
+		JCheckBox blue = new JCheckBox("Blue");
+		JLabel difference = new JLabel("Select a colour to use the difference colour method.");
 
 		button.setPreferredSize(new Dimension(150, 50));
 		choose.setPreferredSize(new Dimension(150, 50));
@@ -240,10 +248,8 @@ boolean removeRed, removeGreen, removeBlue;
 		blue.setPreferredSize(new Dimension(50, 50));
 		difference.setPreferredSize(new Dimension(300, 50));
 
-
 		JPanel panel = new JPanel();
-		
-		
+
 		panel.add(choose);
 		panel.add(difference);
 		panel.add(red);
@@ -251,74 +257,60 @@ boolean removeRed, removeGreen, removeBlue;
 		panel.add(blue);
 		panel.add(button);
 
+		frame.add(panel, BorderLayout.CENTER);
 
-		frame.add(panel , BorderLayout.CENTER);
-		
-		choose.addActionListener(new ActionListener()
-		{
-			  public void actionPerformed(ActionEvent e)
-			  {
+		choose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				// FileChooser
 
-			        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+				JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
-					int returnValue = jfc.showOpenDialog(null);
-					// int returnValue = jfc.showSaveDialog(null);
+				int returnValue = jfc.showOpenDialog(null);
+				// int returnValue = jfc.showSaveDialog(null);
 
-					if (returnValue == JFileChooser.APPROVE_OPTION) {
-						File selectedFile = jfc.getSelectedFile();
-						System.out.println(selectedFile.getAbsolutePath());
-					}
-			  }
-			});
-		button.addActionListener(new ActionListener()
-		{
-			  public void actionPerformed(ActionEvent e)
-			  {
-			    // display/center the jdialog when the button is pressed
-			    JDialog d = new JDialog(frame, "Starting...", true);
-			    d.setLocationRelativeTo(frame);
-			    d.setVisible(true);
-			    //close JFrame
-			    frame.dispose();
-			  }
-			});
-		
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = jfc.getSelectedFile();
+					System.out.println(selectedFile.getAbsolutePath());
+				}
+			}
+		});
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// display/center the jdialog when the button is pressed
+				JDialog d = new JDialog(frame, "Starting...", true);
+				d.setLocationRelativeTo(frame);
+				d.setVisible(true);
+				// close JFrame
+				frame.dispose();
+			}
+		});
+
 		removeRed = false;
 		removeGreen = false;
 		removeBlue = false;
 
-		
-		red.addActionListener(new ActionListener()
-		{
-			  public void actionPerformed(ActionEvent e)
-			  {
-					removeRed = true;
+		red.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeRed = true;
 
-			  }
-			});
-		green.addActionListener(new ActionListener()
-		{
-			  public void actionPerformed(ActionEvent e)
-			  {
-					removeGreen = true;
+			}
+		});
+		green.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeGreen = true;
 
-			  }
-			});
-		
-		blue.addActionListener(new ActionListener()
-		{
-			  public void actionPerformed(ActionEvent e)
-			  {
-					removeBlue = true;
+			}
+		});
 
-			  }
-			});
-		
-        frame.setVisible(true);
+		blue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeBlue = true;
 
-    }
-    
-    
+			}
+		});
+
+		frame.setVisible(true);
+
+	}
 
 }
